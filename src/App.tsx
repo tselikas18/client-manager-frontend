c881e396ae340c8000439af13e7f6f9b83161633
Georgios Tselikas <georgios.p.tselikas@gmail.com>
2025-08-25 20:28:39 +0300

    feat (app): completed the routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import AuthProvider from "./components/AuthProvider.tsx";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router";
+import LoginPage from "./components/LoginPage.tsx";
+import Dashboard from "./components/Dashboard.tsx";
+import Clients from "./components/Clients.tsx";
+import Suppliers from "./components/Suppliers.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/auth/login" element={<LoginPage/>} />
+            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+            <Route path="/clients" element={<ProtectedRoute><Clients /></ProtectedRoute>} />
+            <Route path="/suppliers" element={<ProtectedRoute><Suppliers /></ProtectedRoute>} />
+            <Route path="/" element={<Navigate to="/clients" replace />} />
+            {/*<Route path="/" element={<Navigate to="/dashboard" replace />} />*/}
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+  );
 }
 
 export default App
